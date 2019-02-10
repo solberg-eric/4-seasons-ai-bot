75bec88bf2d1fb45123443780e25013f1fa318b9
Eric Solberg <ericsolberg@Erics-MBP.wi.rr.com>
2019-02-10 16:57:26 -0600

    Started to document more with docstrings.

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-"""Main"""
+""" Main script. """
 
 import setup
 import winner
 import trick
 import hand
 
-depth = 1
-    
-game = setup.setup_game(depth)
+# 'depth_factor' means "how many tricks the minimax algorithm searches."
+depth_factor = 1
+ 
+# 'game' is a dictionary that stores important values used throughout the program.   
+game = setup.setup_game(depth_factor)
 
+# Until the game has ended, continue looping (playing the game).
 while not winner.has_won(game):
+    # Until a particular hand is not finished, continue looping (playing tricks).
     while not game["hand_finished"]:
-        trick.play_trick(game)
+        trick.play_trick(game) 
     hand.setup_next_hand(game)
 
+# Conclude game.
 winner.declare_winner(game)
--- setup.py
+++ setup.py
@@ -1,39 +1,57 @@
-"""Instantiates necessary variables and prepares for game"""
+""" Prepares for game. """
 
 # From main
 def setup_game(depth_factor):
+    """Prepares for game.
+
+    Args: 
+        depth_factor (int): The depth of search (in tricks) for the minimax algorithm.
+        
+    Returns: 
+        game (dict): Stores frequently used values pertaining to the game.
+    """
     class Player:
         def __init__(self, hand, cards_in_play, cards_in_play_ranks, trick_pile, cost, gain, has_won):
-            self.hand = hand
-            self.cards_in_play = cards_in_play
-            self.cards_in_play_ranks = cards_in_play_ranks
-            self.trick_pile = trick_pile
-            self.cost = cost
-            self.gain = gain
-            self.has_won = has_won
+            self.hand = hand # (list) of (strings): Cards in hand.
+            self.cards_in_play = cards_in_play # (list) of (strings): Cards in play.
+            self.cards_in_play_ranks = cards_in_play_ranks # (list of (ints): Value of cards in play.
+            self.trick_pile = trick_pile # (list) of (strings): Cards won/captured by player.
+            self.cost = cost # (int): The 'value' that a player loses from their hand with they play.
+            self.gain = gain # (int): The 'value' that a player gains when they win a trick.
+            self.has_won = has_won # (boolean): 'True' if the player has won the game, false otherwise.
 
+    # Creates Player One (p1) and Player Two (p2) with initial startingi values.
     p1 = Player(["AH", "KH", "QH", "JH", "AS", "KS", "QS", "JS", "AD", "KD", "QD", "JD", "AC", "KC", "QC", "JC"], [], [0, 0], [], 0, 0, False)
     p2 = Player(["AH", "KH", "QH", "JH", "AS", "KS", "QS", "JS", "AD", "KD", "QD", "JD", "AC", "KC", "QC", "JC"], [], [0, 0], [], 0, 0, False)
 
+    # Stores frequently used values pertaining to the game.
     game = {
         "p1" : p1,
         "p2" : p2,
-        "depth_factor" : depth_factor,
-        "depth" : depth_factor * 4,
-        "alpha" : -float("Inf"),
-        "beta" : float("Inf"),
+        "depth_factor" : depth_factor, # Depth of search for minimax algoirthm (in tricks)
+        "depth" : depth_factor * 4, # Depth of search for minimax algorithm (in turns). There are four turns per trick.
+        "alpha" : -float("Inf"), # Used in minimax algorithm
+        "beta" : float("Inf"), # Used in minimax algorithm
         "trump_order" : ["Hearts", "Spades", "Diamonds", "Clubs"],
-        "lead_player" : lead_player(),
-        "lead_suit" : None, # "H", "S", "D", or "C"
-        "player_winning_trick" : None, # Eventually going to be "p1" or "p2"
+        "lead_player" : lead_player(), # (string): "p1" for player one; "p2" for player two.
+        "lead_suit" : None, # (string): "H", "S", "D", or "C".
+        "player_winning_trick" : None, # Eventually (string): "p1" or "p2".
         "hand_finished" : False,
-        "value" : 0,
-        "player_optimizing" : "p1"
+        "value" : 0, # (int): Value of potential game state (used in evaluation function during minimax algorithm).
+        "player_optimizing" : "p1" # (string): Either "p1" or "p2". Denotes which player is being optimized in minimax algorithm.
     }
     return game
 
 # From self.setup_game
 def lead_player():
+    """Asks the user who the lead player of the game is. 
+    
+    Args: 
+        none
+    
+    Returns: 
+        (string): 'p1' or 'p2'.
+    """
     while True:
         response = input("Are you the starting player? (y/n): ")
         print()
